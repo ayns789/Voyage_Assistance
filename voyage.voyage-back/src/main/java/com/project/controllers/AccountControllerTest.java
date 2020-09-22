7ed8f5917da0bb0c4e70fe52f37556dd0f84814e
ayns789 <joly.a.idf@gmail.com>
2020-09-22 23:01:55 +0200

    up

--- voyage.voyage-back/src/main/java/com/project/controllers/AccountControllerTest.java
+++ voyage.voyage-back/src/main/java/com/project/controllers/AccountControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.project.services.CalculationServiceTest;
